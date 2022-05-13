6d63cbe3642db56bfe583ed643d504ba5b6a34cb
Abhinav <abhinavrajputaes@gmail.com>
2022-05-13 14:27:41 +0530

    Implemented face recognition and improved ui

--- README.md
+++
@@ -1,9 +0,0 @@
-# IMPORTANT: Bug Fixes
-
-## `navigator.getUserMedia`
-
-`navigator.getUserMedia` is now deprecated and is replaced by `navigator.mediaDevices.getUserMedia`. To fix this bug replace all versions of `navigator.getUserMedia` with `navigator.mediaDevices.getUserMedia`
-
-## Low-end Devices Bug
-
-The video eventListener for `play` fires up too early on low-end machines, before the video is fully loaded, which causes errors to pop up from the Face API and terminates the script (tested on Debian [Firefox] and Windows [Chrome, Firefox]). Replaced by `playing` event, which fires up when the media has enough data to start playing.
--- js/script.js
+++ js/script.js
@@ -17,15 +17,21 @@ function startVideo() {
 
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
+  document.getElementById("face").append(canvas)
+  const displaySize = { width: video.width, height: 560 }
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
 })
+
+/* DARK MODE CODE */
+const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+	document.getElementById('bck').classList.toggle('dark');
+});
